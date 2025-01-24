815105231105b9f088658432c8662c146fabb150
XavierPs90 <xavialonsops@gmail.com>
2025-01-24 14:44:05 -0600

    valida input y crea lista de nombres

--- app.js
+++ app.js
@@ -1,1 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+
+// VARIABLES
+
+let amigos = [];
+let nombre = "";
+let btnAgregar = document.getElementById("btnAgregar");
+let elementoNombre = document.getElementById("amigo");
+let listaAmigos = document.getElementById("listaAmigos");
+
+// FUNCIONES ESPECIFICAS
+
+function validarCampoVacío(nombre) {
+  let valida = false;
+  
+  if(nombre == ""){
+    valida = false;
+  }
+  else {
+    valida = true;
+  }
+
+  return valida;
+}
+
+function crearListaAmigos(amigos, listaAmigos) {
+  listaAmigos.innerHTML = "";
+
+  for (let i = 0; i < amigos.length; i++) {
+    const nombre = amigos[i];
+    listaAmigos.innerHTML += `<li>${nombre}</li>`;
+  }
+}
+
+// CODIGO PRINCIPAL
+
+btnAgregar.addEventListener("click", function(){
+  nombre = document.getElementById("amigo").value;
+
+  if(validarCampoVacío(nombre)){
+    amigos.push(nombre);
+    elementoNombre.value = "";
+  }
+  else{
+    alert("Por favor, inserte un nombre");
+  }
+
+  crearListaAmigos(amigos, listaAmigos);
+});
+
